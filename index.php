10364bd76780be9ab7d0f0ee667f3a121436fbaf
sainath1601 <36723037+sainath1601@users.noreply.github.com>
2018-03-17 22:11:49 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>JOB</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+
+
+  
+  <link rel='stylesheet prefetch' href='https://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.css'>
+
+      <link rel="stylesheet" href="css/style.css">
+
+  <script type="text/javascript">
+        function func(){
+          var title=document.getElementById("title").value;
+          var job=document.getElementById("location").value;
+          var exp=document.getElementById("exp").value;
+          console.log("JOB TITLE:"+title+"\nJOB LOCATION:"+job+"\nEXPERIENCE:"+exp);
+        }
+      </script>
+</head>
+
+<body>
+
+  <div class="container">
+  <div class="row">
+    <div class="Absolute-Center is-Responsive">
+ 
+      <div class="col-sm-12 col-md-10 col-md-offset-1">
+        <form action="" id="loginForm" method="POST">
+          <div class="form-group input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+            <input class="form-control" type="text" id="title" name='JOBTITLE' placeholder="JOBTITLE"/>          
+          </div>
+          <div class="form-group input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+            <input class="form-control" type="text" id="location" name='JOBLOCATION' placeholder="JOBLOCATION"/>     
+          </div>
+          <div class="form-group input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+            <input class="form-control" type="text" id="exp" name='EXPERIENCE' placeholder="EXPERIENCE"/>     
+          </div>
+      
+          <div class="form-group">
+            <button type="submit" class="btn btn-def btn-block" onclick="func()">submit</button>
+          </div>
+          
+        </form>        
+      </div>  
+    </div>    
+  </div>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js'></script>
+
+  
+
+</body>
+
+</html>
